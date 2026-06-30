910b00ba561bde7b05fa06f1323e60e929677ea6
Lucas Gabriel Do Nascimento <lucas.gabriel.nascimento.br@gmail.com>
2026-06-29 21:40:29 -0300

    Exercicios

---
+++ 01-variaveis/ex003.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia dois números e mostre a soma entre eles
+
+parcelas_1 = int(input("Parcelas: "))
+parcelas_2 = int(input("Parcelas: "))
+soma = parcelas_1 + parcelas_2
+print(f"{parcelas_1} + {parcelas_2} = {soma}")
